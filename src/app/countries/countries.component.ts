bad7c1f4dbd2b8200cc1a38bc681693b5a434278
Aisoku San <aisoku@MacBook-Pro-de-Aisoku.local>
2020-10-03 12:52:55 -0500

    Country Pipe

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,22 +1,31 @@
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Country } from './../interface/country.interfaces';
+import { Country } from '@myApp/models';
 import * as countries from './../../assets/countries.json';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
+  providers: [TitleCasePipe]
 })
 export class CountriesComponent implements OnInit {
 
   selected: Country;
+  alpha3Code: string;
 
-  constructor() { 
+  constructor(private titleCasePipe: TitleCasePipe) { 
     this.selected = countries[10];
     console.log(this.selected);
   }
 
   ngOnInit(): void {
+  }
+  send(value: string) : void{
+    console.log("Antes ::: ", value);
+    value = this.titleCasePipe.transform(value);
+    console.log("Despues ::: ", value);
+
   }
 
 }
